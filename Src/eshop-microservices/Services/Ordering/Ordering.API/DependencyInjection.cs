b6422df8a077a2f257e06fa055e124471ba08979
Trung VH <vhtrung52@gmail.com>
2025-01-14 00:38:35 +0700

    #8 create project

---
+++ Src/eshop-microservices/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            return app;
+        }
+    }
+}
