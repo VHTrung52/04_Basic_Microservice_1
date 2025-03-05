933a424a6bb5b70f1f4f8aafb5c05e6061f22db4
Trung Vu <vhtrung52@gmail.com>
2025-03-05 16:22:39 +0700

    #8: add command and query handler

---
+++ Src/eshop-microservices/Services/Ordering/Ordering.API/DependencyContextAssemblyCatalogCustom.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using Carter;
+
+namespace Ordering.API;
+
+public class DependencyContextAssemblyCatalogCustom : DependencyContextAssemblyCatalog
+{
+    public override IReadOnlyCollection<Assembly> GetAssemblies()
+    {
+        return new List<Assembly> { typeof(Program).Assembly };
+    }
+}
